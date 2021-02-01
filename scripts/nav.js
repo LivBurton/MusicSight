69aeebf1dd3a89ff50e0dfa24d57fd7b745f60c2
LivBurton <ojburton@yahoo.co.uk>
2021-02-01 09:57:58 +0000

    navigation changed

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,12 +1,9 @@
 // ----------NAVBAR TOGGLE---------------
 
-document.addEventListener('load', function () {
-  console.log('whatever');
-});
 const mainNav = document.getElementById('main-nav');
 const navToggle = document.getElementById('nav-toggle');
 
 navToggle.addEventListener('click', function () {
   mainNav.classList.toggle('show');
-  console.log(mainNav.offsetHeight);
+  // console.log(mainNav.offsetHeight);
 });
